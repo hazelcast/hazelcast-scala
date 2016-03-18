c2b00298c66369a754d4017e1e15670b0fcab6d8
nilskp <nilskp@gmail.com>
2016-03-18 13:35:11 -0500

    Remove implicit conversion warning.

--- src/main/scala/com/hazelcast/Scala/xa/package.scala
+++ src/main/scala/com/hazelcast/Scala/xa/package.scala
@@ -1,6 +1,7 @@
 package com.hazelcast.Scala
 
 import com.hazelcast.core.HazelcastInstance
+import language.implicitConversions
 
 package object xa {
   implicit def inst2xa(hz: HazelcastInstance): XAHazelcastInstance = new XAHazelcastInstance(hz)
