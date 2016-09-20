820a3697710f85094c75eade296f30151a8d75de
nilskp <nilskp@gmail.com>
2016-09-20 13:02:05 -0500

    Fix scaladoc warnings

--- src/main/scala/com/hazelcast/Scala/AsyncRingbuffer.scala
+++ src/main/scala/com/hazelcast/Scala/AsyncRingbuffer.scala
@@ -19,8 +19,8 @@ class AsyncRingbuffer[E](private val rb: Ringbuffer[E]) extends AnyVal {
   /**
     * Add item.
     * @param item The item to add
-    * @param overflowPolicy The overflow policy, defaults to [[OVERWRITE]]
-    * @return The sequence number added. Will only return `None` if default policy is [[FAIL]] and capacity is reached.
+    * @param overflowPolicy The overflow policy, defaults to `OVERWRITE`
+    * @return The sequence number added. Will only return `None` if default policy is `FAIL` and capacity is reached.
     */
   def add(item: E, overflowPolicy: OverflowPolicy = OverflowPolicy.OVERWRITE): Future[Option[Long]] = {
     rb.addAsync(item, overflowPolicy).asScala
@@ -29,8 +29,8 @@ class AsyncRingbuffer[E](private val rb: Ringbuffer[E]) extends AnyVal {
   /**
     * Add batch of items.
     * @param items The items to add
-    * @param overflowPolicy The overflow policy, defaults to [[OVERWRITE]]
-    * @return The last sequence number added. Will only return `None` if default policy is [[FAIL]] and capacity is reached.
+    * @param overflowPolicy The overflow policy, defaults to `OVERWRITE`
+    * @return The last sequence number added. Will only return `None` if default policy is `FAIL` and capacity is reached.
     */
   def addAll(items: Iterable[E], overflowPolicy: OverflowPolicy = OverflowPolicy.OVERWRITE): Future[Option[Long]] = {
     rb.addAllAsync(items.asJavaCollection, overflowPolicy).asScala
